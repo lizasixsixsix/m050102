d3fb6bad3e5fe0492c7f163f702b18928daf95dc
lizasixsixsix <lizasixsixsix@gmail.com>
2018-04-18 10:30:14 +0300

    attribute signed types registration added

--- m050102/Container.cs
+++ m050102/Container.cs
@@ -1,6 +1,34 @@
-﻿namespace m050102
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using m050102.Attributes;
+
+namespace m050102
 {
     public class Container
     {
+        public HashSet<Type> Registrants { get; set; }
+
+        public Container()
+        {
+        }
+
+        public Container(params Assembly[] assemblies)
+        {
+            foreach (var a in assemblies)
+            {
+                this.RegisterAssembly(a);
+            }
+        }
+
+        private void RegisterAssembly(Assembly assembly)
+        {
+            this.Registrants = new HashSet<Type>(
+                assembly.GetTypes()
+                    .Where(t => t.GetCustomAttributes(typeof(ExportAttribute),
+                                        false)
+                                    .Any()));
+        }
     }
 }
